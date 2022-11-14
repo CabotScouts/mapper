a9daab1d5093501a76bef95aace8fd3fea537c80
Owen Jones <owen@owenjones.net>
2022-11-14 14:42:03 +0000

    more bins; more units (need to get these read from a csv also)

--- mapper/__init__.py
+++ mapper/__init__.py
@@ -22,8 +22,8 @@ class PostcodeMapper(object):
     recache = False
 
     # Number of bins for histogram - smaller = less resolution
-    xbins = 25
-    ybins = 25
+    xbins = 40
+    ybins = 40
 
     tiledepth = 16
 
@@ -96,13 +96,13 @@ class PostcodeMapper(object):
     def getUnits(self):
         units = numpy.array(
             [
+                [51.472855, -2.584198, "red"], # Arizona
                 [51.4786941724208, -2.598413827911372, "red"],  # Brabazon
-                [51.455440, -2.620430, "purple"], # Clifton
                 [51.486130609067075, -2.589508893981929, "orange"],  # Concorde
                 [51.47463134806349, -2.583211067214961, "orange"],  # Phoenix
                 [51.512744513854194, -2.6151856808814955, "green"],  # Pirates
                 [51.487019158751714, -2.6184016494903517, "orange"],  # Spaniorum
-                [51.46631052970262, -2.600189450279231, "orange"],  # Spitfire
+                [51.46631052970262, -2.600189450279231, "green"],  # Spitfire
                 [51.48641788642314, -2.6240557460937453, "red"],  # Steama
                 [51.48279004181177, -2.6097863941345167, "orange"],  # White Tree
             ]
